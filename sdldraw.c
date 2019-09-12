95b450092518057693eccb82cdaafa62ece33248
Thomas Haschka <haschka@gmail.com>
2019-09-12 22:22:00 +0200

    implemented color bor - no switching yet - and Bresenham's algorithm

--- sdldraw.c
+++ sdldraw.c
@@ -71,7 +71,8 @@ void draw_full_color_palette_image(unsigned int *palette_image,
     for(j=0;j<40;j++) {
       for(k=0;k<40;k++) {
 	pixel_i = palette_image+j*640+start+k;
-
+	pixel_c = (unsigned char*)pixel_i;
+	
 	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 
 	pixel_c[0] = c.b;
@@ -164,7 +165,48 @@ void color_out_image(unsigned int *image,
 
 #endif
   }
-} 
+}
+
+color get_color_by_click_on_colors_window(int x_offset, color* palette) {
+  return(palette[x_offset/40]);
+}
+
+void sharp_pencil_tool(unsigned int* image, int width, int height,
+		       int x, int y, int prev_x, int prev_y,
+		       int prev_pos_avail, color c) {
+  
+  int dx,sx;
+  int dy,sy;
+  
+  int err;
+  int err_times_two; 
+
+  if(prev_pos_avail) {
+
+    /* simple implementation of Bresenham's Algorithm */
+    
+    dx = abs(x-prev_x);   
+    dy = -abs(y-prev_y);  
+    sx = x < prev_x ? 1 : -1;
+    sy = y < prev_y ? 1 : -1;
+
+    err = dx+dy;          
+    err_times_two = 2*err;
+    
+    while (1) {
+      change_pixel_color_in_image(image,width,height,x,y,c.r,c.g,c.b);
+	if(x == prev_x && y == prev_y) break;
+      err_times_two = 2*err;
+      if(err_times_two > dy) {err += dy; x+=sx;}
+      if(err_times_two < dx) {err += dx; y+=sy;}
+    }
+
+  } else {
+    change_pixel_color_in_image(image,width,height,x,y,c.r,c.g,c.b);
+  }
+}
+
+    
 
 int main(int argc, char** argv) {
 
@@ -172,10 +214,14 @@ int main(int argc, char** argv) {
   unsigned int* colors_frame;
   
   unsigned int width=640, height=480;
+  unsigned int x, y;
   
   SDL_Window *image_window = NULL;
   SDL_Window *tools_window = NULL;
   SDL_Window *colors_window = NULL;
+
+  unsigned int image_window_id, tools_window_id, colors_window_id;
+
   SDL_Renderer *image_renderer = NULL;
   SDL_Renderer *tools_renderer = NULL;
   SDL_Renderer *colors_renderer = NULL;
@@ -183,9 +229,19 @@ int main(int argc, char** argv) {
   SDL_Event event;
   SDL_Texture *image_texture;
   SDL_Texture *color_texture;
-  unsigned int pitch, color_pitch=640*480;
+  unsigned int pitch = width*4, color_pitch=640*4;
 
   int mouse_down = 0;
+
+  int prev_pos_x;
+  int prev_pos_y;
+  int prev_pos_avail;
+  
+  color selected_color = {0,0,0};
+  color* palette = gen_initial_palette();
+
+  void (*current_tool)(unsigned int*, int, int, int, int, int, int, int, color);
+  current_tool = &sharp_pencil_tool;
   
   SDL_SetMainReady();
 
@@ -197,6 +253,7 @@ int main(int argc, char** argv) {
   image_window = SDL_CreateWindow("Image",100, 20, width, height, 0);
   tools_window = SDL_CreateWindow("Tools",10, 20, 80, 480, 0);
   colors_window = SDL_CreateWindow("Colors",100, 520, 640, 40, 0);
+
   
 #ifdef SOFTWARE_RENDERING
   image_renderer = SDL_CreateRenderer(image_window,-1,SDL_RENDERER_SOFTWARE);
@@ -215,6 +272,10 @@ int main(int argc, char** argv) {
 				    SDL_TEXTUREACCESS_STREAMING,
 				    640,40);
 
+
+  image_window_id = SDL_GetWindowID(image_window);
+  tools_window_id = SDL_GetWindowID(tools_window);
+  colors_window_id = SDL_GetWindowID(colors_window);
   
   SDL_LockTexture(image_texture,NULL,(void**)&image_frame, &pitch);
   color_out_image(image_frame,width,height,255,255,255);
@@ -225,7 +286,7 @@ int main(int argc, char** argv) {
   SDL_RenderPresent(image_renderer);
 
   SDL_LockTexture(color_texture,NULL,(void**)&colors_frame, &color_pitch);
-  draw_full_color_palette_image(colors_frame,gen_initial_palette());
+  draw_full_color_palette_image(colors_frame,palette);
   SDL_UnlockTexture(color_texture);
 
   SDL_RenderCopy(colors_renderer,color_texture,NULL,NULL);
@@ -241,9 +302,17 @@ int main(int argc, char** argv) {
 	break;
       case SDL_MOUSEBUTTONDOWN:
 
-	mouse_down = 1;
-	break;
+	if(event.button.windowID == image_window_id) {
+	  mouse_down = 1;
+	  prev_pos_avail = 0;
+	}
 	
+	if(event.button.windowID == colors_window_id) {
+	  selected_color = get_color_by_click_on_colors_window(event.button.x,
+							       palette);
+	}
+	break;
+	  
       case SDL_MOUSEBUTTONUP:
 	
 	mouse_down = 0;
@@ -252,18 +321,36 @@ int main(int argc, char** argv) {
     }
 
     if(mouse_down) {
-      SDL_LockTexture(image_texture,NULL,(void**)&image_frame, &pitch);
-      change_pixel_color_in_image(image_frame,width,height,
-				  event.motion.x,
-				  event.motion.y,0,0,0);
-      SDL_UnlockTexture(image_texture);
-      SDL_RenderCopy(image_renderer,image_texture,NULL,NULL);
+      
+      x = event.motion.x;
+      y = event.motion.y;
+      if(x > 0 && x < width && y > 0 && y < height) {
+	SDL_LockTexture(image_texture,NULL,(void**)&image_frame, &pitch);
+
+	(*current_tool)(image_frame, width, height,
+		        x, y, prev_pos_x, prev_pos_y,
+		        prev_pos_avail, selected_color);
+	
+	SDL_UnlockTexture(image_texture);
+	SDL_RenderCopy(image_renderer,image_texture,NULL,NULL);
+	prev_pos_avail = 1; 
+      } else {
+	/* no previous position available if cursur is went off screen */
+	prev_pos_avail = 0; 
+      }
+      prev_pos_x = x;
+      prev_pos_y = y;
+
     }
+
     
     SDL_Delay(10);
     SDL_RenderPresent(image_renderer);
     SDL_RenderPresent(colors_renderer);
   }
  finish:
+  SDL_DestroyWindow(image_window);
+  SDL_DestroyWindow(tools_window);
+  SDL_DestroyWindow(colors_window);
   return(0);
 }
